22172ee071fbe31500c8545dc367817753061051
Wajid Kagzi <wjkagzi@gmail.com>
2015-07-25 12:58:14 -0400

    Challenge 2 completed.

--- learnyounode.js
+++ learnyounode.js
@@ -1,2 +1,9 @@
 // Challenge 1
-console.log("HELLO WORLD");
+// console.log("HELLO WORLD");
+
+// Challenge 2
+var result = 0
+for (var i = 2; i < process.argv.length; i++) {
+  result += Number(process.argv[i]);
+};
+console.log(result);
