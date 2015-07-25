fbceaee7563ad60386ee82b38054039b6bd50a99
Wajid Kagzi <wjkagzi@gmail.com>
2015-07-25 12:59:05 -0400

    Challenge 3 completed.

--- learnyounode.js
+++ learnyounode.js
@@ -2,8 +2,14 @@
 // console.log("HELLO WORLD");
 
 // Challenge 2
-var result = 0
-for (var i = 2; i < process.argv.length; i++) {
-  result += Number(process.argv[i]);
-};
-console.log(result);
+// var result = 0
+// for (var i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i]);
+// };
+// console.log(result);
+
+// Challenge 3
+var fs = require('fs');
+var buf = fs.readFileSync(process.argv[2]);
+var str = buf.toString().split('\n').length - 1;
+console.log(str);
